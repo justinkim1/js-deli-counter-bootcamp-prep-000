4c72e87cc0d8eda103317b4cb66b172fbd4868f5
Justin K <justin-kim1@hotmail.com>
2017-12-25 02:06:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function takeANumber(line,name){
+<<<<<<< HEAD
   line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
@@ -16,5 +17,12 @@ var lineNow = [];
 for(var i=0; i < line.length; i++) {
     lineNow.push(i+1 + ". "+ line[i]);
   }
+<<<<<<< HEAD
   return "The line is currently: " + lineNow.join(', ');
 }
+=======
+=======
+  return 
+>>>>>>> fbdf453b855cbe538e62ea867d3edd072871ac98
+}
+>>>>>>> 40e083ae303259928e0d148e27c907ec383fb408
